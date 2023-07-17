c19db90b23ce23efa2ab9b950700a7b80e45fe6d
Eaglorn <lexus1.45@mail.ru>
2023-07-17 13:51:48 +1000

    Init commit

---
+++ src/boot/luxon.js
@@ -0,0 +1,9 @@
+import { boot } from 'quasar/wrappers';
+import { DateTime } from 'luxon';
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$datetime = DateTime;
+});
+
+export { DateTime };
+
---
+++ src/stores/vico-store.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia';
+
+export const useVicoStore = defineStore('vico', {
+  state: () => ({
+    vicos: [],
+  }),
+  getters: {
+    getVicoById: (state) => {
+      return (vicoId) => state.vicos.find((vico) => vico.id === vicoId);
+    },
+  },
+});
